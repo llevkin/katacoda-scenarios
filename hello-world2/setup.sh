3dcc0af120ab50bdc2e5711ba2d6e335fdbebc53
Leonid Levkin <llevkin@yandex.ru>
2020-04-24 10:37:13 +0300

    hello world

--- hello-world2/setup.sh
+++ hello-world2/setup.sh
@@ -1,1 +1,2 @@
-#mkdir -p example; cd example/
+curl -L https://github.com/llevkin/katacoda-scenarios/tree/master/hello-world2/data.tar.gz > ~/data.tar.gz
+tar -xvzf ~/data.tar.gz -C /home/scrapbook/tutorial
