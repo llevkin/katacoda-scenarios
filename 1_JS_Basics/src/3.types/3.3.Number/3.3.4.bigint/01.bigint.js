b6950ebbcdc234e8e9568d260813e7d35a332349
Leonid Levkin <llevkin@yandex.ru>
2020-04-24 19:20:44 +0300

    dev

--- 1_JS_Basics/src/3.types/3.3.Number/3.3.4.bigint/01.bigint.js
+++ 1_JS_Basics/src/3.types/3.3.Number/3.3.4.bigint/01.bigint.js
@@ -1,5 +1,16 @@
 let a = 10n,
-    b = 10;
+    b = 10,
+    c = 1.5;
 
-console.log(a == b, a === b);
-console.log(typeof a, typeof b);
+console.log(1, a == b, a === b);
+console.log(2, a > c);
+console.log(3, typeof a, typeof b);
+
+try {
+    console.log(b + a);
+}
+catch (e) {
+    console.log(4, `Error: ${e.message}`);
+}
+
+console.log(BigInt(b) + a);
