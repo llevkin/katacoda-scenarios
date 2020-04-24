8306bef558fa2fb5ac041e17d7b00a0fd40c936a
Leonid Levkin <llevkin@yandex.ru>
2020-04-24 16:22:08 +0300

    dev

--- 1_JS_Basics/setup.sh
+++ 1_JS_Basics/setup.sh
@@ -1,2 +1,2 @@
-curl -L https://github.com/llevkin/katacoda-scenarios/blob/master/1_JS_Basics/src.tar.gz?raw=true > ~/src.tar.gz
+curl -L -H 'Cache-Control: no-cache' https://github.com/llevkin/katacoda-scenarios/blob/master/1_JS_Basics/src.tar.gz?raw=true > ~/src.tar.gz
 tar -xvzf ~/src.tar.gz -C /home/scrapbook/
