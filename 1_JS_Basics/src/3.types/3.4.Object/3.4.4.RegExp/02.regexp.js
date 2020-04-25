8d50cbe3d51e33806c86f99de5c64864b75a6bb6
Leonid Levkin <llevkin@yandex.ru>
2020-04-25 15:53:13 +0300

    dev

--- 1_JS_Basics/src/3.types/3.4.Object/3.4.4.RegExp/02.regexp.js
+++ 1_JS_Basics/src/3.types/3.4.Object/3.4.4.RegExp/02.regexp.js
@@ -1,5 +1,10 @@
-let exp = /[abc]{1}\d+/igm;
+let exp = /^[abc]{1}(\d+)$/,
+    res1 = 'a1'.match(exp),
+    res2 = 'ba1'.match(exp),
+    res3 = 'c'.match(exp),
+    res4 = 'c123'.match(exp);
 
-res = exp.test(str);
-
-console.log(exp, res);
+console.log(res1);
+console.log(res2);
+console.log(res3);
+console.log(res4);
