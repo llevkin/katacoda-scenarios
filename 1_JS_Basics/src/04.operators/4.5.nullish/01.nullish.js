59e92946c0b40cad2e92378917a69d95847fb59a
Leonid Levkin <llevkin@yandex.ru>
2020-04-27 15:54:37 +0300

    dev

---
+++ 1_JS_Basics/src/04.operators/4.5.nullish/01.nullish.js
@@ -0,0 +1,24 @@
+let a, b, c, d, e,
+    object = {
+        a: 10,
+        b: false,
+        c: undefined,
+        d: null,
+        e: NaN,
+    };
+
+a = object.a || 1;
+b = object.b || true;
+c = object.c || 2;
+d = object.d || 3;
+e = object.e || 4;
+
+console.log(a, b, c, d, e);
+
+a = object.a ?? 1;
+b = object.b ?? true;
+c = object.c ?? 2;
+d = object.d ?? 3;
+e = object.e ?? 4;
+
+console.log(a, b, c, d, e);
