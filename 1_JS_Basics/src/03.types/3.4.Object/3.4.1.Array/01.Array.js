607cd1f08aa52593d462975ef0b4f1d52858dfa8
Leonid Levkin <llevkin@yandex.ru>
2020-05-05 11:08:45 +0300

    refactor

--- 1_JS_Basics/src/03.types/3.4.Object/3.4.1.Array/01.Array.js
+++ 1_JS_Basics/src/03.types/3.4.Object/3.4.1.Array/01.Array.js
@@ -1,11 +1,3 @@
 let arr = [1, 2, 3];
 
 console.log(arr, arr.length);
-
-console.log('\nИтерация, пример 1');
-for (let i = 0; i < arr.length; i++)
-    console.log(i, arr[i]);
-
-console.log('\nИтерация, пример 2');
-for (let value of arr)
-    console.log(value);
