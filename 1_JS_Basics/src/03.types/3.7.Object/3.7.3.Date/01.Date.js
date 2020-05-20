633896dbb4f3cc5e599f2fc35fbabb59996452df
Leonid Levkin <llevkin@yandex.ru>
2020-05-20 13:14:04 +0300

    struct fix

--- 1_JS_Basics/src/03.types/3.7.Object/3.7.3.Date/01.Date.js
+++ 1_JS_Basics/src/03.types/3.7.Object/3.7.3.Date/01.Date.js
@@ -6,6 +6,10 @@ date = new Date('2020-04-01T12:10:12.568Z');
 
 console.log(date);
 
+date = new Date('December 17, 1995 03:24:00');
+
+console.log(date);
+
 date = new Date(1587806164075);
 
 console.log(date);
