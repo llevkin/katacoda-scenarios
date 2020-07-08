2a7ec4c744f757e3dd3aabc4040696f11a7ccb82
Leonid Levkin <llevkin@yandex.ru>
2020-07-08 10:47:50 +0300

    part 2

--- 2_JS_Extended/setup.sh
+++ 2_JS_Extended/setup.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/llevkin/katacoda-scenarios.git tutorial;
 #ln -s /root/tutorial/2_JS_Extended/src /root/data/src;
-cp -r /root/tutorial/2_JS_Extended/src /root/data/;
+cp -r /root/tutorial/2_JS_Extended/src/ /root/data/;
 cd /root/data/src;
