35c6dada350d286260f4051612927c85d13a1483
Leonid Levkin <llevkin@yandex.ru>
2020-07-21 14:21:45 +0300

    GC

---
+++ 2_JS_Extended/src/04.GC/01.GC.js
@@ -0,0 +1,24 @@
+let i    = 0;
+let arr  = [];
+let wait = time => new Promise(resolve => setTimeout(resolve, time));
+
+function add(value) {
+    arr.push(value);
+}
+
+function memory(text) {
+    global.gc();
+    console.log(text + ' ' + (process.memoryUsage().heapUsed / Math.pow(1024, 2)).toFixed(2) + 'mb');
+}
+
+memory('init:');
+
+for (; i < 1e5; i++)
+    add(new Array(10000).join('some big data'));
+
+memory(`(GC) link exists [items: ${i}]:`);
+
+arr = null;
+
+memory(`(GC) link not exists [items: ${i}]:`);
+
